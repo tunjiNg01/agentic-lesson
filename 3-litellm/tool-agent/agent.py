9d451d7a333233548491a505d943c4176786ba35
tunjiNg01 <nahtlosng@gmail.com>
2025-05-13 22:06:12 +0100

    feat/ add other models

--- 3-litellm/tool-agent/agent.py
+++ 3-litellm/tool-agent/agent.py
@@ -1,5 +1,14 @@
 from google.adk.agents import Agent
 from google.adk.tools import google_search
+from datetime import datetime
+def get_current_time():
+    
+    """
+     Get the current time in the format "YYYY-MM-DD HH:MM:SS".
+    """
+    return {
+      "current_time":  datetime.now().strftime("%Y-%m-%d %H:%M:%S")        
+    }
 
 root_agent = Agent(
     name="tool_agent",
@@ -7,9 +16,9 @@ root_agent = Agent(
     description="An agent that greets the user",
     instruction="""
     You are a helpfull assistant that can use the following tools:
-    - google_search
+    - get_current_time
     """,
     tools=[
-        google_search
+        get_current_time
     ]
     )
--- README.md
+++ README.md
@@ -9,3 +9,21 @@ In this lesson I learnt the following:
 - The folder structure of an agent
 - How to setup agent with absic param
 - Some ADK comand
+
+## 2-tools
+
+In this lesson I learnt the following:
+
+- How I can add different tool to LLM
+- Creating and adding custom tools to LLM
+- When writing a function to be paased in to the ADK as a tools
+  The funtion doc need to be included
+
+## 3-litellm
+
+In this lesson I learnt the following:
+
+- I dicover LiteLLM which is part of adk that allow us interract with agent using single API
+- I learnt how to use other model with ADK
+- https://openrouter.ai
+- https://www.litellm.ai/
